57a8289b8cf5189898b0eb27177f2e98a419a928
xkylz1 <fauzansaputra741@gmail.com>
2024-09-27 19:27:19 +0800

    refactor: server

--- server/index.js
+++ server/index.js
@@ -3,23 +3,12 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-// Serve static files from the 'public' directory
-// app.use(express.static('public'));
-
 app.use(express.static(path.join(__dirname, "../public")));
 
-// Handle requests to the root ("/") by sending index.html automatically
-// app.get("/", (req, res) => {
-//   res.sendFile(path.join(__dirname, "../public/index.html"));
-// });
 app.get("/findcar", (req, res) => {
-  console.log("test");
   res.sendFile(path.join(__dirname, "../public/pages/findcar.html"));
 });
-// app.get("/findcar", (req, res) => {
-//   res.sendFile(path.join(__dirname, "public", "pages", "findcar.html"));
-// });
-// Set up the server to listen on a specific port
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
